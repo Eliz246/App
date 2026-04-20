5badf65ed1bf523f46de652d196bd45ff917be43
Eliz <liezetta.246@gmail.com>
2026-04-20 20:51:15 +0300

    card_wrapp_font commmit

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,20 @@ function App() {
   return (
   <>
     <Header></Header>
-    <div className="container">
-      <Card id='0' image={houseImg} title="Название проекта" area={100} floors={1} rooms={5}/>
+    <div className="product-card">
+      <div className="container">
+      <h2 className='h2'>Наши проекты</h2>
+      <div className="card_wrapper">
+        <Card id='0' image={houseImg} title="Название проекта" area={100} floors={1} rooms={5}/>
+        <Card id='1' image={houseImg} title="Название проекта" area={100} floors={1} rooms={5}/>
+        <Card id='2' image={houseImg} title="Название проекта" area={100} floors={1} rooms={5}/>
+        <Card id='3' image={houseImg} title="Название проекта" area={100} floors={1} rooms={5}/>
+        <Card id='4' image={houseImg} title="Название проекта" area={100} floors={1} rooms={5}/>
+        <Card id='5' image={houseImg} title="Название проекта" area={100} floors={1} rooms={5}/>
+        <Card id='5' image={houseImg} title="Название проекта" area={100} floors={1} rooms={5}/>
+        <Card id='5' image={houseImg} title="Название проекта" area={100} floors={1} rooms={5}/>
+      </div>
+      </div>
     </div>
   </>
     
