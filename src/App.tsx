0b56af4431739f08d2957a23bb4c6553590b1fc7
Eliz <liezetta.246@gmail.com>
2026-04-20 18:14:06 +0300

    card commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import './index.css'
 import Header from './components/ui/Header/Header';
+import Card from './components/ui/Card/Card';
 function App() {
   return (
+  <>
     <Header></Header>
+    <Card id='0' image='./images/house-isolated-field.jpg' title="название проекта" area={100} floors={1} rooms={5}/>
+  </>
+    
   )
 }
 
---
+++ src/components/ui/Card/Card.tsx
@@ -0,0 +1,62 @@
+import React, {useState} from "react";
+import s from './Card.module.css';
+import heartS from '../../../images/heart_stroke_b.svg';
+import heartF from '../../../images/heart_fill_b.svg';
+
+interface CardProps{
+    id: string | number;
+    image:string;
+    title:string;
+    area:number;
+    floors:number;
+    rooms: number;
+};
+
+const Card =({id, image, title, area, floors, rooms}:CardProps)=>{
+    const [isFavorite, setIsFavorite] = useState(() => {
+        const saved = localStorage.getItem('favorites');
+        const favorites = saved ? JSON.parse(saved) : [];
+        return favorites.includes(id);
+    });
+    const toggleFavorite = (e: React.MouseEvent) => {
+        e.stopPropagation();
+        // 2. Получаем текущий список из памяти
+        const saved = localStorage.getItem('favorites');
+        let favorites = saved ? JSON.parse(saved) : [];
+
+        if (!isFavorite) {
+        // Добавляем ID, если его там нет
+        favorites.push(id);
+        } else {
+        // Удаляем ID, если он там был
+        favorites = favorites.filter((favId: string | number) => favId !== id);
+        }
+        // 3. Сохраняем обновленный список обратно
+        localStorage.setItem('favorites', JSON.stringify(favorites));
+        setIsFavorite(!isFavorite);
+    };
+    const declOfNum = (number: number, titles: [string, string, string]) => {
+        const cases = [2, 0, 1, 1, 1, 2];
+        return titles[number % 100 > 4 && number % 100 < 20 ? 2 : cases[Math.min(number % 10, 5)]];
+    };
+    return(
+        <div className={s.card}>
+            <div className={s.img_wrapper}>
+                <img className={s.img} src={image} alt={title}/>
+                <button className={s.favoriteBtn} onClick={toggleFavorite}>
+                    <img src={isFavorite ? heartF : heartS} alt="favorite" className={s.heartIcon}/>
+                </button>
+            </div>
+            <div className={s.content}>
+                <h4 className={s.title}>{title}</h4>
+                <div className={s.info}>
+                    <p className={s.item}>{area}m2</p>
+                    <p className={s.item}>{floors}{declOfNum(floors,[' этаж',' этажа',' этажей'])}</p>
+                    <p className={s.item}>{rooms}{declOfNum(rooms,[' комната',' комнаты',' комнат'])}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
